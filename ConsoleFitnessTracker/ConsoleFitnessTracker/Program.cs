1c05fb8b7dae2d60a0f7dd753264e4094864c8e9
Michael Christy <emonomics@gmail.com>
2016-12-02 15:07:52 -0500

    Initial commit with mockup

---
+++ ConsoleFitnessTracker/ConsoleFitnessTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFitnessTracker
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Prompt user for minutes exercised 
+            // Add minutes exercised to total 
+            // Display total minutes exercised to the screen 
+            // Repeat until user quits    
+        }
+    }
+}
