70111fad9e8cbf0c96d71fe62cd9f5305274f744
Michael Christy <emonomics@gmail.com>
2016-12-04 14:03:14 -0500

    Added loop to keep program running and quit command when user types quit into the consol. also added running total of time.

--- ConsoleFitnessTracker/ConsoleFitnessTracker/Program.cs
+++ ConsoleFitnessTracker/ConsoleFitnessTracker/Program.cs
@@ -10,15 +10,33 @@ namespace ConsoleFitnessTracker
     {
         static void Main()
         {
-            // Prompt user for minutes exercised 
-            Console.Write("Enter how many minutes you exercised: ");
+            int runningTotal = 0;
+            bool keepGoing = true;
 
-            string input = Console.ReadLine();
+            while (keepGoing)
+            { 
+                // Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you exercised or type quit to exit: ");
+                string input = Console.ReadLine();
+                if (input == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    int minutes = int.Parse(input);
 
-            // Add minutes exercised to total 
-            // Display total minutes exercised to the screen 
-            Console.WriteLine("You've exercised for " + input + " minutes.");
-            // Repeat until user quits    
+                    // Add minutes exercised to total 
+                    runningTotal = runningTotal + minutes;
+
+                    // Display total minutes exercised to the screen 
+                    Console.WriteLine("You've exercised for " + runningTotal + " minutes.");
+
+                    // Repeat until user quits
+                }
+                
+            }
+            Console.WriteLine("Goodbye!");
         }
     }
 }
